18fe12b29c8c41a8744bf3d41a0bd2c889601375
Bruce Atkinson <bruce@appinlet.com>
2018-12-31 18:42:14 +0200

    = 1.0.4 - December 31, 2018 =
    * Add auto-update feature

--- wp-e-commerce-gateway-paygate.php
+++ wp-e-commerce-gateway-paygate.php
@@ -1,9 +1,9 @@
 <?php
-/*
+/**
  * Plugin Name: PayGate PayWeb3 plugin for WP eCommerce
- * Plugin URI: https://www.paygate.co.za
+ * Plugin URI: https://github.com/PayGate/PayWeb_WP_eCommerce
  * Description: Accept payments for WP eCommerce using PayGate's PayWeb3 service
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: PayGate (Pty) Ltd
  * Author URI: https://www.paygate.co.za/
  * Developer: App Inlet (Pty) Ltd
@@ -30,6 +30,37 @@ function paygate_activation()
     $theme_dir   = $plugin_path . 'wp-e-commerce/wpsc-merchants/paygate.php';
     if ( !copy( $plugin_dir, $theme_dir ) ) {
         echo "failed to copy $plugin_dir to $theme_dir...\n";
+    }
+
+    /**
+     * Auto updates from GIT
+     *
+     * @since 1.0.4
+     *
+     */
+
+    require_once 'library/updater.class.php';
+
+    if ( is_admin() ) {
+        // note the use of is_admin() to double check that this is happening in the admin
+
+        $config = array(
+            'slug'               => plugin_basename( __FILE__ ),
+            'proper_folder_name' => 'wp-e-commerce-gateway-paygate',
+            'api_url'            => 'https://api.github.com/repos/PayGate/PayWeb_WP_eCommerce',
+            'raw_url'            => 'https://raw.github.com/PayGate/PayWeb_WP_eCommerce/master',
+            'github_url'         => 'https://github.com/PayGate/PayWeb_WP_eCommerce',
+            'zip_url'            => 'https://github.com/PayGate/PayWeb_WP_eCommerce/archive/master.zip',
+            'homepage'           => 'https://github.com/PayGate/PayWeb_WP_eCommerce',
+            'sslverify'          => true,
+            'requires'           => '4.0',
+            'tested'             => '4.9.8',
+            'readme'             => 'README.md',
+            'access_token'       => '',
+        );
+
+        new WP_GitHub_Updater( $config );
+
     }
 }
 
